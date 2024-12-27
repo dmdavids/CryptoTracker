6c0fecceb426284ded485f346534e87169b7ac42
Daun Davids <daundavids@gmail.com>
2024-12-26 20:55:13 -0600

    Coin List screen video 5 adding events part 1

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coinlist/CoinListScreen.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coinlist/CoinListScreen.kt
@@ -1,6 +1,7 @@
 package com.plcoding.cryptotracker.crypto.presentation.coinlist
 
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,19 +12,55 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import com.plcoding.cryptotracker.R
+import com.plcoding.cryptotracker.core.domain.util.toString
 import com.plcoding.cryptotracker.crypto.presentation.coinlist.components.CoinListItem
 import com.plcoding.cryptotracker.crypto.presentation.coinlist.components.previewCoin
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.withContext
 
 @Composable
 fun CoinListScreen(
     state: CoinListState,
+    events: Flow<CoinListEvent>,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    // this will only run once
+    LaunchedEffect(true) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            // This is the workaround to ensure that events are not lost
+            // which can cause a state mismatch
+            withContext(Dispatchers.Main.immediate) {
+                events.collect { event ->
+                    when (event) {
+                        is CoinListEvent.Error -> {
+                            Toast.makeText(
+                                context,
+                                event.error.toString(context),
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
     if (state.isLoading) {
         Box(
             modifier = modifier
@@ -61,6 +98,7 @@ private  fun CoinListScreenPreview() {
                     previewCoin.copy(id = it.toString())
                 }
             ),
+            events = emptyFlow(),
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.background)
         )
--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coinlist/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coinlist/CoinListViewModel.kt
@@ -6,8 +6,10 @@ import com.plcoding.cryptotracker.core.domain.util.onError
 import com.plcoding.cryptotracker.core.domain.util.onSuccess
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
 import com.plcoding.cryptotracker.crypto.presentation.models.toCoinUi
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -16,6 +18,9 @@ class CoinListViewModel(
 ): ViewModel() {
     private val _state = MutableStateFlow(CoinListState())
     val state = _state.asStateFlow()
+
+    private val _events = Channel<CoinListEvent>()
+    val events = _events.receiveAsFlow()
 
     init {
         loadCoins()
@@ -49,6 +54,7 @@ class CoinListViewModel(
                 }
                 .onError { error ->
                     _state.update { it.copy(isLoading = false)}
+                    _events.send(CoinListEvent.Error(error))
                 }
         }
     }
